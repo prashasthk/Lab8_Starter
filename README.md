4f344199ce14fa060cb270e3bb2f3096d39e86f3
Prash Katukojwala <31807747+prashasthk@users.noreply.github.com>
2023-05-26 23:46:55 -0700

    Expose and Explore

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab 8 - Starter
+
+Name: Prashasth Katukojwala <br>
+
+Check Your Understanding <br>
+1. Within a Github action that runs whenever code is pushed because you want to automatically and iteratively test your software everytime new changes are pushed to the repository to make sure everything is working as intended (continuous integration).
+2. No, we would not use an end to end test because it is meant to test user actions instead of unit testing where we would check if a function is returning the correct output.
+3. I would not use a unit test for the message feature because it is not an individual component and interacts with many other components so that a user is able to send a message and another user is able to receive it.
+4. I would use a unit test for the "max message length" feature because it is testing an individual part of my code that checks and prevents the user from typing more than 80 characters.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,72 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// isPhoneNumber unit tests
+test('Tests valid phone number (212)456-7890', () => {
+    expect(functions.isPhoneNumber('(212)456-7890')).toBe(true);
+});
+test('Tests valid phone number (858)578-5600', () => {
+    expect(functions.isPhoneNumber('(858)578-5600')).toBe(true);
+});
+test("Test invalid phone number 8-88888", () => {
+    expect(functions.isPhoneNumber('8-88888')).toBe(false);
+});
+test("Test invalid phone number 222", () => {
+    expect(functions.isPhoneNumber('222')).toBe(false);
+});
+
+// isEmail unit tests
+test('Tests valid email simple@example.com', () => {
+    expect(functions.isEmail('simple@example.com')).toBe(true);
+});
+test('Tests valid email abc@example.co', () => {
+    expect(functions.isEmail('abc@example.co')).toBe(true);
+});
+test('Tests invalid email paul.example.com', () => {
+    expect(functions.isEmail('paul.example.com')).toBe(false);
+});
+test('Tests invalid email paul@.com', () => {
+    expect(functions.isEmail('paul@.com')).toBe(false);
+});
+
+// isStrongPassword unit tests
+test('Tests strong password HelloWorld', () => {
+    expect(functions.isStrongPassword('HelloWorld')).toBe(true);
+});
+test('Tests strong password Hello123_', () => {
+    expect(functions.isStrongPassword('Hello123_')).toBe(true);
+});
+test('Tests weak password #123', () => {
+    expect(functions.isStrongPassword('#123')).toBe(false);
+});
+test('Tests weak password thisisareallylongpassword', () => {
+    expect(functions.isStrongPassword('thisisareallylongpassword')).toBe(false);
+});
+
+// isDate unit tests
+test('Tests valid date 05/25/2023', () => {
+    expect(functions.isDate('05/25/2023')).toBe(true);
+});
+test('Tests valid date 1/1/3000', () => {
+    expect(functions.isDate('1/1/3000')).toBe(true);
+});
+test('Tests invalid date 05/25/23', () => {
+    expect(functions.isDate('05/25/23')).toBe(false);
+});
+test('Tests invalid date 2023/25/05', () => {
+    expect(functions.isDate('2023/25/05')).toBe(false);
+});
+
+// isHexColor unit tests
+test('Tests valid hex #008000', () => {
+    expect(functions.isHexColor('#008000')).toBe(true);
+});
+test('Tests valid hex #FFFFFF', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+test('Tests invalid hex #fff1az', () => {
+    expect(functions.isHexColor('#fff1az')).toBe(false);
+});
+test('Tests invalid hex #00', () => {
+    expect(functions.isHexColor('#00')).toBe(false);
+});
